dc3661235645404428b9f846497180d606732b69
state303 <state303@dsub.io>
2026-07-31 18:00:41 +0900

    fix: encode dump manifest download path

--- src/data/e2e_test.go
+++ src/data/e2e_test.go
@@ -11,7 +11,8 @@ import (
 	"time"
 )
 
-var e2eChecksumManifestURL = DiscogsS3BaseUrl + "data/2026/discogs_20260701_CHECKSUM.txt"
+const e2eChecksumManifestURL = "https://data.discogs.com/?download=" +
+	"data%2F2026%2Fdiscogs_20260701_CHECKSUM.txt"
 
 func TestDiscogsChecksumManifestIsReachable(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
